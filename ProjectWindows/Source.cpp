03f42617dac997c1828364400ab7bf19249d47f0
Марія Гребенчук <grebenchuk.mariia2004@outlook.com>
2024-02-12 13:34:23 +0200

    ..

--- ProjectWindows/Source.cpp
+++ ProjectWindows/Source.cpp
@@ -2,8 +2,8 @@
 // которые используются при написании приложений под Windows. 
 #include <windows.h>
 #include <tchar.h>
-
-
+#include <ctime>
+#define _CRT_SECURE_NO_WARNINGS
 
 INT WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
 {/*
@@ -48,6 +48,67 @@ INT WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, i
     wsprintf(avgCharOnPageWindow, TEXT("Среднее количество символов на странице: %d"), avgCharOnPage);
     MessageBox(0, TEXT("Последняя страница резюме"), avgCharOnPageWindow, MB_OK | MB_ICONINFORMATION);
 */
+
+
     //2
+  
+
+    srand(time(NULL));
+    int attempts = 0;
+    int randNum = rand() % 100 + 1;
+    int guessedNum;
+    bool game = true;
+    do {
+     
+        guessedNum = 0;
+        attempts++;
+
+        int result = MessageBox(0, TEXT("Угадайте число от 1 до 100."), TEXT("Угадай число"), MB_ICONQUESTION | MB_OKCANCEL);
+        if (result == IDCANCEL) {
+            MessageBox(0, TEXT("Вы отменили игру."), TEXT("Конец игры"), MB_OK | MB_ICONINFORMATION);
+            game = false;
+            break;
+        }
+
+     
+       
+        TCHAR buffer[100]; 
+
+        result = MessageBox(0, TEXT("Введите ваше число:"), TEXT("Угадай число"), MB_ICONQUESTION | MB_OKCANCEL);
+        if (result == IDCANCEL) {
+            MessageBox(0, TEXT("Вы отменили игру."), TEXT("Конец игры"), MB_OK | MB_ICONINFORMATION);
+            game = false;
+            break;
+        }
+        if (result == IDOK) {
+            _stprintf_s(buffer, TEXT("%d"), result); 
+             guessedNum = _tstoi(buffer);
+             MessageBox(0, TEXT("Ваше число было принято."), TEXT("Угадай число"), MB_ICONINFORMATION | MB_OK);
+
+        }
+      
+     
+       
+
+
+        if (guessedNum < randNum) {
+            MessageBox(0, TEXT("Загаданное число больше."), TEXT("Подсказка"), MB_ICONINFORMATION | MB_OK);
+        }
+        else if (guessedNum > randNum) {
+            MessageBox(0, TEXT("Загаданное число меньше."), TEXT("Подсказка"), MB_ICONINFORMATION | MB_OK);
+        }
+        else {
+            MessageBox(0, TEXT("Загаданное число найдено."), TEXT("Поздравляем!"), MB_ICONINFORMATION | MB_OK);
+            game = false;
+        }
+
+    } while (game);
+
+    TCHAR message[100];
+    wsprintf(message, TEXT("Поздравляем! Вы угадали число за %d попыток. Хотите сыграть еще раз?"), attempts);
+    int choice = MessageBox(0, message, TEXT("Победа!"), MB_ICONQUESTION | MB_YESNO);
+    if (choice == IDYES) {
+        _tWinMain(hInst, hPrevInst, lpszCmdLine, nCmdShow);
+    }
 
 }
